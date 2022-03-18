883369e9d20b727081ecaca868dd4e20766be98e
Laith Alalamat <98953541+LaithAlamat@users.noreply.github.com>
2022-03-18 18:34:47 +0300

    Update UnitTest1.cs

--- BalanceTest/UnitTest1.cs
+++ BalanceTest/UnitTest1.cs
@@ -12,5 +12,43 @@ namespace BalanceTest
             decimal getBalance = Program.ViewBalance();
             Assert.Equal(getBalance, Program.ViewBalance());
         }
+        [Fact]
+        public void CorrectDeposit()
+        {
+            decimal getBalance = Program.ViewBalance();
+            decimal result = getBalance + 1000;
+            Assert.Equal(result, Program.Deposit(1000));
+        }
+
+        [Fact]
+        public void IncorrectDeposit()
+        {
+            decimal getBalance = Program.ViewBalance();
+            decimal result = getBalance;
+
+            Assert.NotEqual(result, Program.Deposit(100));
+
+        }
+        [Fact]
+        public void CorrectWithdraw()
+        {
+            Program.Deposit(500);
+            decimal getBalance = Program.ViewBalance();
+            decimal result = getBalance - 50;
+
+            Assert.Equal(result, Program.Withdraw(50));
+        }
+
+        [Fact]
+        public void IncorrectWithdraw()
+        {
+            Program.Deposit(500);
+            decimal getBalance = Program.ViewBalance();
+            decimal result = getBalance;
+
+            Assert.NotEqual(result, Program.Withdraw(100));
+
+        }
+
     }
 }
